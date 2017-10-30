449f29c776713c0e3baeabd1ac3820fffce3ed95
Emily Jordan <emily.h.jordan@gmail.com>
2017-10-30 17:01:08 -0400

    post disucssion routes

--- server/api/products.js
+++ server/api/products.js
@@ -3,29 +3,32 @@ const {Product, Category} = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
-  Product.findAll()
+  Product.findAll({include: [{ model: Category }]})
     .then(products => res.json(products))
     .catch(next)
 })
 
-router.get('/:categoryId', (req, res, next) => {
-  Category.findOne({
-    where: {id: req.params.categoryId}
-  }).then(category => res.json(category))
-  .catch(next)
-})
+// router.get('/:categoryId', (req, res, next) => {
+//   Product.findAll({
+//     where: {categoryId: req.params.categoryId}
+//   })
+//   .then(products => res.json(products))
+//   .catch(next)
+// })
 
 router.get('/:productId', (req, res, next) => {
   Product.findOne({
     where: {id: req.params.productId}
-  }).then(product => res.json(product))
+  })
+  .then(product => res.json(product))
   .catch(next)
 })
 
 router.get('/:productName', (req, res, next) => {
   Product.findOne({
     where: {title: req.params.productName}
-  }).then(product => res.json(product))
+  })
+  .then(product => res.json(product))
   .catch(next)
 })
 
@@ -39,6 +42,7 @@ router.post('/', (req, res, next) => {
 router.put('/:productId', (req, res, next) => {
   Product.update(req.body, {
     where: {id: req.params.id}
-  }).then(() => res.status(200))
+  })
+  .then(() => res.status(200))
   .catch(next)
 })
