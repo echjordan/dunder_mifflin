a55f5a522ac991dcfd4cb4a230d94dd38f618121
EHenry92 <EHenry692@hotmail.com>
2017-11-02 16:52:56 -0400

    working cart reducer and component

--- client/components/index.js
+++ client/components/index.js
@@ -6,4 +6,5 @@
 export {default as Main} from './main'
 export {default as UserHome} from './user-home'
 export {default as Products} from './products'
+export {default as Cart} from './cart'
 export {Login, Signup} from './auth-form'
