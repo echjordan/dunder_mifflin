c148af1f0167e746333d4f2fb0fbcab735348f79
Emily Jordan <emily.h.jordan@gmail.com>
2017-11-06 14:34:31 -0500

    less broken

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -17,37 +17,53 @@ const SingleProduct = (props) => {
     }
 
     return (
-      <div>
-        <h1>{product.title}</h1>
-        <div className="product-image-container">
-        <img className="product-images-1" src={product.photos[0]} />
+
+      <div className="card">
+        <div className="card-content">
+          <h1>{product.title}</h1>
+          <div className="product-image-container">
+            <img className="product-images-1" src={product.photos[0]} />
+          </div>
+          <div className="product-image-container">
+            <img className="product-images-2" src={product.photos[1]} />
+          </div>
+          <p>${product.price}</p>
+          <a className="btn-floating btn-large waves-effect waves-light red" onClick={handleClick} value={product.id}><i className="material-icons">+</i></a>
         </div>
-        <div className="product-image-container">
-        <img className="product-images-2" src={product.photos[1]} />
+        <div className="card-tabs">
+          <ul className="tabs tabs-fixed-width">
+            <li className="tab"><a href="#test4">
+              Description
+            </a></li>
+            <li className="tab"><a className="active" href="#test5">Test 2</a></li>
+            <li className="tab"><a href="#test6">Test 3</a></li>
+          </ul>
         </div>
-        <ul>
-          <li>${product.price}</li>
-          <li>{product.description}</li>
-        </ul>
-        <button className="products-add" onClick={handleClick} value={product.id} >
-          +
-                </button>
-        <ul>
-          CATEGORIES:
-          {
+        <div className="card-content grey lighten-4">
+          <div id="test4">
+            {product.description}
+            Categories:
+            {
             product.categories.map(category => <li key={category.id}>{category.name}</li>)
           }
-        </ul>
-        <ul>
-          REVIEWS: <br />
-          <Link to={`/${product.id}/new-review`}>
-          <button className="add-review-btn" > Post a review </button>
-          </Link>
-          {
-            product.reviews.map(review => <li key={review.id}>{review.title}, {review.content}  STARS: {produceStars(review.stars)}</li>)
-          }
-        </ul>
+          </div>
+          <div id="test6">Test 3</div>
+        </div>
       </div>
+        // <ul>
+        //   CATEGORIES:
+        //
+        // </ul>
+        // <ul>
+        //   REVIEWS: <br />
+        //   <Link to={`/${product.id}/new-review`}>
+        //   <button className="add-review-btn" > Post a review </button>
+        //   </Link>
+        //   {
+        //     product.reviews.map(review => <li key={review.id}>{review.title}, {review.content}  STARS: {produceStars(review.stars)}</li>)
+        //   }
+        // </ul>
+
     )
 }
 
